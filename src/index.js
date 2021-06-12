f004cdb62db3b7c91ce8cef54e4b16ffbaf0a0c9
charao fouguta <mdi.acc@icloud.com>
2021-06-12 22:24:07 +0900

    presentation

--- src/index.js
+++ src/index.js
@@ -20,6 +20,24 @@ if (campaign && campaign == 'register' || campaign && campaign == '202105') {
 		}
 	}
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+	const anchorLinks = document.querySelectorAll('a[href^="#"]');
+	const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+  
+	anchorLinksArr.forEach(link => {
+	  link.addEventListener('click', e => {
+		e.preventDefault();
+		const targetId = link.hash;
+		const targetElement = document.querySelector(targetId);
+		const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+		window.scrollTo({
+		  top: targetOffsetTop,
+		  behavior: "smooth"
+		});
+	  });
+	});
+  });
 /*
 getWidth();
 window.onresize = getWidth;

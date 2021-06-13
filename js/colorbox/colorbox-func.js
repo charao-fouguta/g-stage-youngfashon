d1544dcc5b31e6dfd9ce15a6c25295e63399e2d8
charao fouguta <mdi.acc@icloud.com>
2021-06-13 20:54:17 +0900

    [fix] images

--- js/colorbox/colorbox-func.js
+++ js/colorbox/colorbox-func.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	//Examples of how to assign the Colorbox event to elements
 	$(".cboxBannerSP").colorbox({inline:true,rel:'cboxBannerSP',maxWidth:"75%",maxHeight:"95%",transition:"fade"});
 	$(".cboxBannerPC").colorbox({inline:true,rel:'cboxBannerPC',maxWidth:"95%",maxHeight:"95%",transition:"fade"});
-	$(".group1").colorbox({rel:'group1',maxWidth:"90%",maxHeight:"90%",transition:"fade"});
-	$(".group2").colorbox({rel:'group2',maxWidth:"90%",maxHeight:"90%",transition:"fade"});
-	$(".group3").colorbox({rel:'group3',maxWidth:"90%",maxHeight:"90%",transition:"fade"});
-	$(".group4").colorbox({rel:'group4',maxWidth:"90%",maxHeight:"90%",transition:"fade"});
+	$(".group1").colorbox({maxWidth:"90%",maxHeight:"90%",transition:"fade"});
+	$(".group2").colorbox({maxWidth:"90%",maxHeight:"90%",transition:"fade"});
+	$(".group3").colorbox({maxWidth:"90%",maxHeight:"90%",transition:"fade"});
+	$(".group4").colorbox({maxWidth:"90%",maxHeight:"90%",transition:"fade"});
 });
